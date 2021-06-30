a37b2ef076638812bca3658777a60c5ac0071642
milaneuh <milanrgmnt@gmail.com>
2021-06-30 13:36:40 +0200

    Mod

--- morpion.java
+++ morpion.java
@@ -21,6 +21,13 @@ public class morpion
         System.out.println(case7 + separation + case8 + separation + case9);
     }
 
+    public static int choseGame(){
+        Scanner choser = new Scanner(System.in);
+        System.out.println("Comment voulez-vous jouer ? J vs J ( 0 ) ? J vs Ia ( 1 ) ? Ia vs Ia  ( 2 )?");
+        int chosenGame = choser.nextInt();
+        return  chosenGame;
+    }
+
     public static void play() //Fonction permettant de récupérer la case d'un joueur
     {
         Scanner player = new Scanner(System.in); //Création du Scanner
@@ -28,120 +35,157 @@ public class morpion
         boolean win = false;
         String joueur = "";
         char resultValue = 'r';
-
-        while (win == false) //On joue tant que on a pas de gagnant
+        int gameMode = choseGame();
+        System.out.println("Vous avez choisi :" + gameMode);
+        while (!win) //On joue tant que on a pas de gagnant
         {
-            if(turnCount %2 == 0 || turnCount == 0) //Systeme de joueur 1 et 2
+            boolean rejouer = false;
+            if (turnCount % 2 == 0) //Systeme de joueur 1 et 2
             {
-                joueur = "Joueur 1";
+                if (gameMode == 0 || gameMode == 1) {
+                    joueur = "Joueur 2";
+                } else {
+                    joueur = "IA Joueur 1";
+                }
                 resultValue = 'O';
-            }
-            else
-            {
-                joueur = "Joueur 2";
+            } else {
+                if (gameMode == 0) {
+                    joueur = "Joueur 2";
+                } else {
+                    joueur = "IA Joueur 2";
+                }
                 resultValue = 'X';
             }
 
-            System.out.println(joueur + ", c'est à vous ! \n"+"Veuillez entrer un numéro de case compris entre 1 et 9. \n\n");
-            int casePlayed = player.nextInt();
-
-            if (casePlayed >= 10 || casePlayed <= 0)
-            //Verification de la validité de la case
-            {
-                System.out.println("Veuillez jouer une case valide");
+            int casePlayed = 0;
+            if (gameMode == 0 || gameMode == 1 && turnCount % 2 == 0) {
+                System.out.println(joueur + ", c'est à vous ! \n" + "Veuillez entrer un numéro de case compris entre 1 et 9. \n\n");
+                casePlayed = player.nextInt();
+                if (casePlayed >= 10 || casePlayed <= 0)
+                //Verification de la validité de la case
+                {
+                    System.out.println("Veuillez jouer une case valide");
+                }
+            } else {
+                //Trouver une valeur de casePlayed en aléatoire
+                System.out.println("C'est au tour de l'"+joueur+". \n");
+                casePlayed = (int) (Math.random() * 8.5) + 1;
             }
 
             switch (casePlayed) {
                 case 1:
-                    if (verifierCase(case1))
-                    {
+                    if (verifierCase(case1)) {
                         case1 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 2:
-                    if (verifierCase(case2))
-                    {
+                    if (verifierCase(case2)) {
                         case2 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 3:
-                    if (verifierCase(case3))
-                    {
+                    if (verifierCase(case3)) {
                         case3 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 4:
-                    if (verifierCase(case4))
-                    {
+                    if (verifierCase(case4)) {
                         case4 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 5:
-                    if (verifierCase(case5))
-                    {
+                    if (verifierCase(case5)) {
                         case5 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 6:
-                    if (verifierCase(case6))
-                    {
+                    if (verifierCase(case6)) {
                         case6 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 7:
-                    if (verifierCase(case7))
-                    {
+                    if (verifierCase(case7)) {
                         case7 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 8:
-                    if (verifierCase(case8))
-                    {
+                    if (verifierCase(case8)) {
                         case8 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
                 case 9:
-                    if (verifierCase(case9))
-                    {
+                    if (verifierCase(case9)) {
                         case9 = resultValue;
+                    }
+                    else
+                    {
+                        rejouer = true;
                     }
                     break;
             }
+
+
             afficherGrille();
 
             //Verification de la victoire, 6 façon de gagner (Ligne ou Diagonales)
-            if(case1 == resultValue && case2 == resultValue && case3 == resultValue ||
+            if (case1 == resultValue && case2 == resultValue && case3 == resultValue ||
                     case4 == resultValue && case5 == resultValue && case6 == resultValue ||
                     case7 == resultValue && case8 == resultValue && case9 == resultValue ||
                     case1 == resultValue && case5 == resultValue && case9 == resultValue ||
                     case3 == resultValue && case5 == resultValue && case7 == resultValue ||
                     case1 == resultValue && case4 == resultValue && case7 == resultValue ||
                     case2 == resultValue && case5 == resultValue && case8 == resultValue ||
-                    case3 == resultValue && case6 == resultValue && case9 == resultValue)
-            {
+                    case3 == resultValue && case6 == resultValue && case9 == resultValue) {
                 win = true;
-                Scanner rejouer = new Scanner(System.in); //Création du Scanner
-                System.out.println(joueur +", vous avez gagné !! :) \n Voulez-vous rejouer? [o/n]");
-                String yesNo = player.nextLine();
-                if(yesNo == "y")
-                {
+                Scanner newGame = new Scanner(System.in); //Création du Scanner
+                System.out.println(joueur + ", vous avez gagné !! :) \n Voulez-vous rejouer? [o/n]");
+                String yesNo = newGame.nextLine();
+                if (yesNo == "y") {
                     play();
-                }
-                else if (yesNo == "n")
-                {
+                } else if (yesNo == "n") {
                     System.out.println("Désolé de vous voir partir :(");
                 }
 
 
-            }
-            else
-            {
+            } else if (!rejouer) {
                 //player.close(); //On ferme le scanner
                 turnCount += 1;
             }
         }
+        }
 
-    }
 
 
     public static boolean verifierCase(char caseAVerifier) //fonction vérifiant si les cases sont déjà remplis
